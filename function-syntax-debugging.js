65168ec91ef40ff38dcf706ff834f07fbdc16105
Jacob Dubbe <129224365+EgregiousDev@users.noreply.github.com>
2024-02-26 18:24:56 -0700

    Add files via upload

---
+++ function-syntax-debugging.js
@@ -0,0 +1,15 @@
+// A student was working on a function and made some 
+// syntax mistakes while coding. Help them find their 
+// mistakes and fix them.
+
+// My solution
+
+function main [verb, noun]
+  return verb + noun
+}
+
+// Changed to
+
+function main (verb, noun){
+    return verb + noun
+  }
