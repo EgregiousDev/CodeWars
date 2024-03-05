d6b16fad940d86519fc7c3087e45abf836129863
Jacob Dubbe <129224365+EgregiousDev@users.noreply.github.com>
2024-03-05 15:31:58 -0700

    Update basic-mathematical-operations.js

--- basic-mathematical-operations.js
+++ basic-mathematical-operations.js
@@ -14,3 +14,20 @@
 function basicOp(operation, value1, value2){
     return eval(value1 + operation + value2)
   }
+
+// Without eval
+
+function basicOp(operation, value1, value2){
+  switch(operation){
+    case '+':
+      return value1 + value2
+    case '-':
+      return value1 - value2
+    case '*':
+      return value1 * value2
+    case '/':
+      return value1 / value2
+    default:
+      return 0
+  }
+}
