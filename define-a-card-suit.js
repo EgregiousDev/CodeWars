04cf2a0ffbdfb4b22a7a112ea93aa4e1ce91ee9d
Jacob Dubbe <129224365+EgregiousDev@users.noreply.github.com>
2024-04-21 21:51:26 -0700

    Add files via upload

---
+++ define-a-card-suit.js
@@ -0,0 +1,22 @@
+// You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+
+// Our deck (is preloaded):
+
+// ('3♣') -> return 'clubs'
+// ('3♦') -> return 'diamonds'
+// ('3♥') -> return 'hearts'
+// ('3♠') -> return 'spades'
+
+// My solution
+
+function defineSuit(card) {
+    if (card.endsWith('♣')){
+      return 'clubs'
+    } else if (card.endsWith('♦')){
+      return 'diamonds'
+    } else if (card.endsWith('♥')){
+      return 'hearts'
+    } else if (card.endsWith('♠')){
+      return 'spades'
+    }
+  }
